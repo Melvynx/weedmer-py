e6a3f460a1a0ecef90992148d43fb9080314e681
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-11-25 11:04:15 +0100

    fix

--- main.py
+++ main.py
@@ -6,26 +6,33 @@
 from encrypt import cipher
 
 message = "Les troupes britanniques sont entrees a Cuxhaven a quatorze heures le six mai Desormais tout le trafic radio cessera je vous souhaite le meilleur Fermeture pour toujours tout le meilleur au revoir."
-crypted = "OSUYCMOYYRGPSSILEDWHXRPKCQLLRLFOMRWQPREIKEFCRROXYGMWHYATPKEITDWVGZNDRKSYFSDMSPCUFUHGDCAVLDZUVFBVSCVXDCWJYUQAAWFYNBWOWECINBLRLZFFERYJWYEOJKGIHNYZAFXYJVMWECZWHGEJLWHATYGTVPZVMPEKIXVDBBKSDSHIFEODIPIBI"
+
+
+def get_cypher(msg):
+    return cipher("I V IV", "B", [13, 15, 11], "NX EC RV GP SU DK IT FY BL AZ", "gyd", msg)
+
 
 def exercise_1():
     # Use a breakpoint in the code line below to debug your script.
 
-    cypher_message = cipher("I V IV", "B", [13, 15, 11], "NX EC RV GP SU DK IT FY BL AZ", "gyd", message)
+    cypher_message = get_cypher(message)
 
     print(cypher_message)  # Press ⌘F8 to toggle the breakpoint.
 
-def exercise_2():
+    return cypher_message
+
+
+def exercise_2(data):
     # Use a breakpoint in the code line below to debug your script.
 
-    cypher_message = cipher("I V IV", "B", [13, 15, 11], "NX EC RV GP SU DK IT FY BL AZ", "gyd", crypted)
+    cypher_message = get_cypher(data)
 
     print(cypher_message)  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    exercise_1()
-    exercise_2()
+    msg = exercise_1()
+    exercise_2(msg)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
